75a240da6e033e98294990daba3723d861f45681
Nishil3567 <88776060+Nishil3567@users.noreply.github.com>
2022-01-11 18:45:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,37 @@
-function preload(){
+noseX = 0;
+noseY = 0;
 
+function preload(){
+    nose = loadImage("https://i.postimg.cc/mDSGt7nb/m.png");
 }
 
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is Iintilized');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x - 170;
+        noseY = results[0].pose.nose.y - 45;
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
 }
 
 function draw(){
+    image(video ,0,0,300,300);
+    image(nose ,noseX,noseY,30,30);
 
 }
 
